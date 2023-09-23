b6f617b5b5f622b8c7f100fb6daf49986a150a2c
abhaykes1 <abhaykesharwani40@gmail.com>
2023-09-23 21:42:55 +0530

    added key managemant changes

--- apps/keymanagement/models.py
+++ apps/keymanagement/models.py
@@ -15,11 +15,12 @@ class LLMProviders(Enum):
 
 class KeyManagement(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(
+    provider = models.CharField(
         max_length=64,
         choices=[(choice.value, choice.name) for choice in LLMProviders],
         default=LLMProviders.OPENAI.value,
     )
+    title = models.TextField(default="")
     description = models.TextField(default="")
     user = models.ForeignKey(User, related_name="keys", on_delete=models.CASCADE)
     api_key = EncryptedTextField()
